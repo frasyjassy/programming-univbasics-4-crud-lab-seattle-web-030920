bfad684bbf49f5d4815adb27bcfb84867cc4461f
Jasmine Ramirez <jsmnramirez25@gmail.com>
2020-02-28 23:22:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,7 +34,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  index_number = "arrays!"
 end
 
 def update_element_from_index(array, index_number, element)
